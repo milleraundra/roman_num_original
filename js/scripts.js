53e722477c06397cdcd46aec83e40525db5dd4f6
milleraundra <miller.aundra@gmail.com>
2016-04-20 17:14:34 -0700

    add conditionals in document.ready

--- js/scripts.js
+++ js/scripts.js
@@ -15,16 +15,31 @@ else {
 			num -= values[i];
 		}
 	}
-	
+
 	romNum = romNum.join("");
 
 	var romNum = romNum.replace(/IIII/g, 'IV');
 	var romNum = romNum.replace(/XXXX/g, 'XL');
 	var romNum = romNum.replace(/CCCC/g, 'CD');
 
-
 return romNum;
 
 	}
 
 }
+
+$(document).ready(function() {
+	$('form#question').submit(function(event) {
+		event.preventDefault();
+		var number = parseInt($('#num').val());
+		console.log(number);
+		if(number >= 4000) {
+			$('#result').text("Please enter a number less than 4000");
+		} else if (number === 0 || number === null) {
+			$('#result').text("Please enter a number");
+		} else {
+			newNumber = romanize(number);
+			$('#result').text("Your number in Roman Numerals is " + newNumber);
+		}
+	})
+})
